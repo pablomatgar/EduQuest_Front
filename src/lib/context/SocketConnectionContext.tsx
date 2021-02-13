55724c252328b2fdab43056a37a83d9a333df91a
Arturo Torres <arturotc22@gmail.com>
2021-02-13 00:51:37 -0600

    Connect to the api

--- src/lib/context/SocketConnectionContext.tsx
+++ src/lib/context/SocketConnectionContext.tsx
@@ -1,5 +1,5 @@
-import React, { useRef } from "react";
-import { Socket } from "socket.io-client";
+import React, { useRef, useEffect } from "react";
+import { Socket, io } from "socket.io-client";
 
 interface ISocketContext {
   socket: Socket | null;
@@ -13,7 +13,7 @@ export const SocketConnectionContext = React.createContext(contextData);
 
 const { Provider } = SocketConnectionContext;
 
-const serverUrl = process.env.REACT_APP_SERVER_URL;
+const serverUrl = process.env.REACT_APP_SERVER_URL as string;
 interface IProps {
   children: JSX.Element;
 }
@@ -23,6 +23,15 @@ interface IProps {
  */
 export function SocketConnectionProvider({ children }: IProps) {
   const socketRef = useRef<Socket | null>(null);
+
+  useEffect(() => {
+    if (socketRef.current === null) {
+      socketRef.current = io(serverUrl, {
+        withCredentials: true,
+      });
+    }
+  }, []);
+
   return (
     <Provider
       value={{
