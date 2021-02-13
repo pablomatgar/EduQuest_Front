3806648a1f911393ab412257a38b3fadf8d248c6
Pablo Mateos García <pmateosga.inf@upsa.es>
2021-02-13 06:31:12 +0100

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import * as React from "react";
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ChakraProvider, theme} from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import { RouteSwitchWithNav } from "./components/RouteSwitchWithNav";
 import { routes } from "./routes";
+import LoginPage from './pages/LoginPage/index';
 
 export const App = () => (
   <BrowserRouter>
     <ChakraProvider theme={theme}>
       <React.Suspense fallback={null}>
         <RouteSwitchWithNav routes={routes} basePath="" />
+        <LoginPage />
       </React.Suspense>
     </ChakraProvider>
   </BrowserRouter>
 );
+
+
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,38 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
+import React, { useState } from "react";
+import * as yup from 'yup';
+import {Formik, Form} from 'formik';
+import { Box, Flex, Heading, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+
+
+let schema = yup.object().shape({
+  email: yup.string().email(),
+  password: yup.string()
+});
+
 
 export function LoginPage() {
   return (
-    <Box>
-      <h1>Login Page</h1>
-    </Box>
+    <Flex width="full" align="center" justifyContent="center">
+      <Box p={8} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg">
+        <Box textAlign="center">
+          <Heading>Login</Heading>
+        </Box>
+        <Box my={4} textAlign="left">
+          <form /*onSubmit={validate}*/>
+            <FormControl isRequired>
+              <FormLabel>Email</FormLabel>
+              <Input type="email" placeholder="mail@mail.com" onChange={event => setEmail(event.currentTarget.value)} />
+            </FormControl>
+            <FormControl mt={6} isRequired>
+              <FormLabel>Password</FormLabel>
+              <Input type="password" placeholder="*******" onChange={event => setPassword(event.currentTarget.value)} />
+            </FormControl>
+            <Button type="submit" colorScheme="teal" variantColor="teal" variant="outline" width="full" mt={4}>
+              Sign In
+            </Button>
+          </form>
+        </Box>
+      </Box>
+    </Flex>
   );
 }
