a4ab12e8c797eb9ae4fdbca59c9444aea1b4610c
Arturo Torres <arturotc22@gmail.com>
2021-02-12 21:52:05 -0600

    Added server url and .env fil

--- src/lib/context/SocketConnectionContext.tsx
+++ src/lib/context/SocketConnectionContext.tsx
@@ -13,10 +13,14 @@ export const SocketConnectionContext = React.createContext(contextData);
 
 const { Provider } = SocketConnectionContext;
 
+const serverUrl = process.env.REACT_APP_SERVER_URL;
 interface IProps {
   children: JSX.Element;
 }
 
+/**
+ * Gives access to the socket if there is an established connection to the server.
+ */
 export function SocketConnectionProvider({ children }: IProps) {
   const socketRef = useRef<Socket | null>(null);
   return (
